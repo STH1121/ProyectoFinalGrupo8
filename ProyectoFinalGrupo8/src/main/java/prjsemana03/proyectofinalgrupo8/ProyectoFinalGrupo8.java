408cc64c35e795f141232f0255ad61d07ef32496
STH1121 <sth11214127@gmail.com>
2023-10-12 11:14:41 -0600

    First Java push

---
+++ ProyectoFinalGrupo8/src/main/java/prjsemana03/proyectofinalgrupo8/ProyectoFinalGrupo8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prjsemana03.proyectofinalgrupo8;
+
+/**
+ *
+ * @author Steven
+ */
+public class ProyectoFinalGrupo8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
